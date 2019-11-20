ace547822b4e7e2db8529841076daf424a621a2b
Ali-hd <ali.hamidaddin1@gmail.com>
2019-11-20 07:31:41 +0300

    finished hw

---
+++ fruits/server.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const methodOverride = require('method-override');
+const Fruit = require('./models/fruits.js');
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/', {useNewUrlParser : true , useUnifiedTopology: true } )
+.then(()=> console.log('Mongodb is running'),(err)=> console.log(err) );
+
+app.set('view engine','ejs')
+
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'));
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    res.send("hellooo")
+})
+
+app.post('/fruits/', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+    Fruit.create(req.body, (error, createdFruit)=>{
+        res.send(createdFruit);
+    });
+});
+
+// app.get('/fruits', (req, res)=>{
+//     res.send('index');
+// });
+
+
+app.get('/fruits', (req, res)=>{
+    Fruit.find({}, (error, allFruits)=>{
+        res.render('index.ejs', {
+            fruits: allFruits
+        });
+    });
+});
+
+// Fruit.create(req.body, (error, createdFruit)=>{
+//     res.redirect('/fruits');
+// });
+
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.send(foundFruit);
+    });
+});
+
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.render('show.ejs', {
+            fruit:foundFruit
+        });
+    });
+});
+
+// Delete Route
+
+// app.delete('/fruits/:id', (req, res)=>{
+//     res.send('deleting...');
+// });
+
+app.delete('/fruits/:id', (req, res)=>{
+    Fruit.findByIdAndRemove(req.params.id, (err, data)=>{
+        res.redirect('/fruits');//redirect back to fruits index
+    });
+});
+
+app.get('/fruits/:id/edit', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{ //find the fruit
+        res.render(
+    		'edit.ejs',
+    		{
+    			fruit: foundFruit //pass in found fruit
+    		}
+    	);
+    });
+});
+
+app.put('/fruits/:id', (req, res)=>{
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    Fruit.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel)=>{
+        res.redirect('/fruits');
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log('server is listening on port 3000')
+})
