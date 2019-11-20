9e223c6541bb01c47ea15b28adbfb9f69b085024
Ahlam Fallatah <f18.ahlam@gmail.com>
2019-11-20 19:47:59 +0300

    homework

--- fruits/server.js
+++ fruits/server.js
@@ -1,57 +1,90 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const Fruit = require('./models/fruits.js');
-
-// create express app
 const app = express();
-
+const mongoose = require('mongoose');
+const Fruit = require('./models/fruits');
+const ejs = require('ejs')
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost/', {useNewUrlParser : true , useUnifiedTopology: true } )
 .then(()=> console.log('Mongodb is running'),(err)=> console.log(err) );
 
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }));
-
-// parse requests of content-type - application/json
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(methodOverride('_method'));
+
 
 // define a simple route
 app.get('/', (req, res) => {
-    // console.log("welcome")
+    res.send("Welcome to fruits website")
 });
 
-//Create Index Route
-app.get('/fruits', (req, res)=>{
-    res.send('index');
-});
-
-//Render the ejs file
-app.get('/fruits', (req, res)=>{
-    res.render('index.ejs');
-});
-
-//Have Index Route Render All Fruits
+//all fruits
 app.get('/fruits', (req, res)=>{
     Fruit.find({}, (error, allFruits)=>{
-        res.render('index.ejs', {
-            fruits: allFruits
-        });
+        res.render('index', {fruits: allFruits} );
     });
 });
 
-app.post('/fruits/', (req, res)=>{
-    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+
+
+//create new fruit
+app.get('/fruits/new',(req,res)=>{
+    res.render('new')
+})
+
+//... and then farther down the file
+app.post('/fruits/', (req, res) => {
+    // console.log(req.body.readyToEat)
+    if (req.body.readyToEat === 'on') { //if checked, req.body.readyToEat is set to 'on'
         req.body.readyToEat = true;
     } else { //if not checked, req.body.readyToEat is undefined
         req.body.readyToEat = false;
     }
     Fruit.create(req.body, (error, createdFruit)=>{
-        res.send(createdFruit);
+        res.redirect('/fruits');
     });
 });
 
-// listen for requests
-app.listen(3000, () => {
-   console.log("Server is listening on port 3000");
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.render('show.ejs', {fruit:foundFruit});
+    });
 });
+
+
+app.delete('/fruits/:id', (req, res)=>{
+    Fruit.findByIdAndRemove(req.params.id, (err, data)=>{
+        res.redirect('/fruits');//redirect back to fruits index
+    });
+});
+
+
+app.get('/fruits/:id/edit', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{ //find the fruit
+        res.render(
+    		'edit.ejs',
+    		{
+    			fruit: foundFruit //pass in found fruit
+    		}
+    	);
+    });
+});
+
+app.put('/fruits/:id', (req, res)=>{
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    Fruit.findByIdAndUpdate(req.params.id, req.body, (err, updatedModel)=>{
+        res.redirect('/fruits');
+    });
+});
+
+
+
+    app.listen(3000, () => {
+        console.log("Server is listening on port 3000");
+     });
--- fruits/views/routes/create.js
+++
@@ -1,3 +0,0 @@
-Fruit.create(req.body, (error, createdFruit)=>{
-    res.redirect('/fruits');
-});
