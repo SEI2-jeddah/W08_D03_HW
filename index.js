cbf2c2c703fcf333cf78f5986e23d85b59554fce
Mohammed Alfisal <mohammedalfisal@Mohammeds-MacBook-Pro.local>
2019-12-10 16:28:53 +0300

    done

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Fruit = require('./models/fruits.js');
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// parse requests of content-type - application/json
+app.use(express.json())
+
+// define a simple route
+app.get('/', (req, res) => {
+res.send('welcome!')
+});
+
+// app.get('/fruits', (req, res)=>{
+//     res.send('index');
+// })
+
+app.get('/fruits', (req, res)=>{
+    res.render('index.ejs');
+});
+
+app.get('/fruits', (req, res)=>{
+    Fruit.find({}, (error, allFruits)=>{
+        res.render('index.ejs', {
+            fruits: allFruits
+        });
+    });
+});
+
+
+
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.send(foundFruit);
+    });
+});
+
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.render('show.ejs', {
+            fruit:foundFruit
+        });
+    });
+});
+
+
+//... and then farther down the file
+app.post('/fruits/', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+    Fruit.create(req.body, (error, createdFruit)=>{
+        res.send(createdFruit);
+    });
+});
+
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+ });
+
+mongoose.connect('mongodb://localhost/', {useNewUrlParser : true , useUnifiedTopology: true } )
+.then(()=> console.log('Mongodb is running'),(err)=> console.log(err) );
