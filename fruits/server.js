dfbdb29fc9a5d7b8e4a7e393bc4543993d987cba
Tareq Alotaibi <tareqdev@protonmail.com>
2019-12-11 09:42:16 +0300

    Finished

--- fruits/server.js
+++ fruits/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const ejs = require('ejs')
 const app = express()
+const methodOverride = require('method-override');
+
 
 // Import models
 const Fruits = require('./models/Fruits')
@@ -12,7 +14,7 @@ app.set("view engine","ejs");
 
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
-
+app.use(methodOverride('_method'));
 
 
 
@@ -50,12 +52,58 @@ app.get('/fruits' , (req ,res ) => {
       });
 })
 
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.render('show.ejs', {
+            fruit:foundFruit
+        });
+    });
+});
 
 
 
+app.delete('/fruits/:id', (req, res)=>{
+    Fruit.findByIdAndRemove(req.params.id, (err, data)=>{
+        res.redirect('/fruits');//redirect back to fruits index
+    });
+});
 
 
 
+app.get('/fruits/:id/edit', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{ //find the fruit
+        res.render(
+    		'edit.ejs',
+    		{
+    			fruit: foundFruit //pass in found fruit
+    		}
+    	);
+    });
+});
+
+
+app.put('/fruits/:id', (req, res)=>{
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    res.send(req.body);
+});
+
+
+
+app.put('/fruits/:id', (req, res)=>{
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    Fruit.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel)=>{
+        res.send(updatedModel);
+        res.redirect('/fruits');
+    });
+});
 
 
 // Port Listening
